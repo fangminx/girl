a78dc1b184587c1df0dd730a38306e82092b1fb3
fangmin <1261869845@qq.com>
2017-08-29 00:09:06 +0800

    用lombok重构日志

--- src/main/java/com/fangminx/aspect/HttpAspect.java
+++ src/main/java/com/fangminx/aspect/HttpAspect.java
@@ -1,9 +1,8 @@
 package com.fangminx.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -12,9 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 
 @Aspect
 @Component
+@Slf4j
 public class HttpAspect {
 
-    private final static Logger logger = LoggerFactory.getLogger(HttpAspect.class);
+//     使用@Slf4j重构
+//    private final static Logger logger = LoggerFactory.getLogger(HttpAspect.class);
 
     //该类下所有方法任何参数都拦截
     @Pointcut("execution(public * com.fangminx.controller.GirlController.*(..))")
@@ -26,17 +27,17 @@ public class HttpAspect {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
 
-        logger.info("url={}",request.getRequestURL());
-        logger.info("method={}",request.getMethod());
-        logger.info("ip={}",request.getRemoteAddr());
+        log.info("url={}",request.getRequestURL());
+        log.info("method={}",request.getMethod());
+        log.info("ip={}",request.getRemoteAddr());
         //类方法
-        logger.info("class_method={}",joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName());
-        logger.info("args={}",joinPoint.getArgs());
+        log.info("class_method={}",joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName());
+        log.info("args={}",joinPoint.getArgs());
     }
 
     @After("log()")
     public void doAfter(){
-        logger.info("2222222222");
+        log.info("2222222222");
     }
 
     /**
@@ -45,6 +46,6 @@ public class HttpAspect {
      */
     @AfterReturning(returning = "object",pointcut = "log()")
     public void doAfterReturning(Object object){
-        logger.info("response={}",object.toString());
+        log.info("response={}",object.toString());
     }
 }
